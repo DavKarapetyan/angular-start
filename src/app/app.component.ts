74b187b539f421f59e172b1df4e2b452cbdd73a6
Davit Karapetyan <davit207karapetyan@gmail.com>
2025-01-02 14:11:08 +0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule, CardModule, ToggleSwitchModule, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ]
+}
   title = 'angularStart';
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('my-app-dark');
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Scan all component templates for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
